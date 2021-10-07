9d92369ee2949b504041142533bea8085a52d5b7
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 10:24:41 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+//change image src with jquery
+$("#myImageID").attr("src","images/my_other_image.png");
+
+//change image src plain javascript
+document.getElementById('myImageID').src="images/my_other_image.png";
